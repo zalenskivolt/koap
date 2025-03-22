e411edfa3a7a91b8d3b0804b9f8c6f39056c6a78
Mattias de Zalenski <zalenski@northvolt.com>
2025-03-23 00:24:07 +0100

    Add missing ending parenthesis in toString methods

--- koap-core/src/commonMain/kotlin/Message.kt
+++ koap-core/src/commonMain/kotlin/Message.kt
@@ -287,7 +287,7 @@ sealed class Message {
 
             override fun hashCode(): Int = etag.contentHashCode()
 
-            override fun toString(): String = "ETag(etag=${etag.toHexString()}"
+            override fun toString(): String = "ETag(etag=${etag.toHexString()})"
         }
 
         /** RFC 7252 5.10.7. Location-Path and Location-Query */
@@ -327,7 +327,7 @@ sealed class Message {
 
             override fun hashCode(): Int = etag.contentHashCode()
 
-            override fun toString(): String = "IfMatch(etag=${etag.toHexString()}"
+            override fun toString(): String = "IfMatch(etag=${etag.toHexString()})"
         }
 
         /** RFC 7252 5.10.8.2. If-None-Match */
